eadad82003df1e780c0a2da3d509337ae675dc00
Данил Говердовский <danilgoverdovskiy@MacBook-Pro-Danil.local>
2021-04-28 23:54:42 +0300

    dgoverd change print

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import db
 
 if __name__ == "__main__":
-    print("aaaaaa")
+    print("a")
     db.check_init_db()
     db.insert('users', {'codename': '2', 'telegram_id': '1234512345'})
     db.insert('categories', {'codename': '1'})
